fee42df5149a4b05bf11765a2d5e7550f1fc8331
Mahmoud-Saadeh <mahmoud.saadeh998@gmail.com>
2021-09-14 14:59:32 +0300

    generate html report 'composition'

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,10 @@
-import { MatchResult } from './MatchResult';
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
+import { Summary } from './Summary';
 
-const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = MatchReader.fromCsv('football.csv');
 
-const matchReader = new MatchReader(csvFileReader);
+const summary = Summary.winsAnalysisWithHtmlReport('Fulham');
+
 matchReader.load();
 
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
